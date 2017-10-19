bbef438522dba14da970c79434e13d3601876868
GwenSW <gwen.simswilliams@gmail.com>
2017-10-20 00:09:35 +0100

    Use XmlHttpRequest to read metadata JSON for specified (hard-coded) story - works in localhost

--- init.js
+++ init.js
@@ -1,1 +1,20 @@
+"use strict";
+
 console.log("init loaded");
+
+const storyName = "roomExplorer";
+
+// maybe a script which loads all the necessary jsons?
+//const json = require([`stories/${storyName}/metadata`]);
+
+const request = new XMLHttpRequest();
+request.open("GET", "http://192.168.0.6:8081/stories/roomExplorer/metadata.json", true);
+
+request.onload = function() {
+  const metadata = JSON.parse(request.responseText);
+  const scene = metadata.story.first_scene;
+  console.log(scene);
+};
+
+request.send();
+
