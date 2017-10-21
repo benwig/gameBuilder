b5ca04c2a6b1193aefed2f1052ddb292b82f1227
GwenSW <gwen.simswilliams@gmail.com>
2017-10-21 17:33:49 +0100

    Create functions in the Ui module for adding and clearing option buttons

--- modules/ui.js
+++ modules/ui.js
@@ -1,12 +1,33 @@
 /*jshint esversion:6, devel: true, browser: true*/
 
-
-
 console.log("start loading ui module");
 
 const Ui = (function () {
   
   "use strict";
+  
+  const prompt = document.querySelector(".storyText"),
+        optlist = document.querySelector(".options");
+  
+  return {
+    
+    addOption: function(text, next) {
+      let opt = document.createElement('li');
+      let button = document.createElement('button');
+      button.dataset.next = `${next}`;
+      button.addEventListener("click", Parse.proceed.bind(null, next));
+      button.textContent = `${text}`;
+      opt.appendChild(button);
+      optlist.appendChild(opt);
+    },
+    
+    clearOptions: function() {
+      while (optlist.lastChild) {
+        optlist.removeChild(optlist.lastChild);
+      }
+    }
+    
+  };
   
   
 })();
