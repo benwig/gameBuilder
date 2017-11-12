530d274b8b33c1ef9c5329a48bba517d251630e2
GwenSW <gwen.simswilliams@gmail.com>
2017-11-12 01:00:59 +0000

    Generate objectives objects from JSON file

--- modules/objectives.js
+++ modules/objectives.js
@@ -5,13 +5,15 @@ const Objectives = (function () {
   "use strict";
   
   const self = {};
-  const objectives = [];
+  const objectives = {};
   
   //constructor function for objectives
-  function Objective(settings) {
+  function Objective (settings) {
     this.text = settings.text;
+    this.type = settings.type;
     this.assigned = false;
     this.completed = false;
+    this.failed = false;
   }
 
   Objective.prototype.changeComplete = function (bool) {
@@ -20,45 +22,59 @@ const Objectives = (function () {
   
   Objective.prototype.changeAssigned = function (bool) {
     this.assigned = bool;
+    if (bool) {
+      this.timeAssigned = Time.get();
+    }
+  };
+  
+  Objective.prototype.fail = function (bool) {
+    this.failed = true;
   };
 
   //////////////////////
   /// PUBLIC METHODS ///
   //////////////////////
   
-  //TODO: init function, for turning the objectives.JSON into named objective objects and pushing them to the objectives array
+  //TODO: init function, for turning the objectives.JSON into named objective objects and pushing them to the objectives object with key "name"
   self.init = function (objectivesJSON) {
-    console.log("Init objectives");
+    let objList = objectivesJSON.objectives;
+    for (let i = 0; i < objList.length; i += 1) {
+      let id = objList[i].id,
+          text = objList[i].text,
+          type = objList[i].type;
+      objectives[id] = new Objective({"text": text, "type": type});
+    }
   };
   
-  self.complete = function (name) {
-    objectives[name].changeComplete(true);
+  self.complete = function (id) {
+    objectives[id].changeComplete(true);
     View.updateObjectives();
   };
   
-  self.uncomplete = function (name) {
-    objectives[name].changeComplete(false);
+  self.uncomplete = function (id) {
+    objectives[id].changeComplete(false);
     View.updateObjectives();
   };
   
-  self.assign = function (name) {
-    objectives[name].changeAssigned(true);
-  }
-  
-  self.getStatus = function (name) {
-    return objectives[name].completed;
+  self.fail = function (id) {
+    objectives[id].fail();
+    View.updateObjectives();
   };
   
-  self.getText = function (name) {
-    return objectives[name].text;
+  self.assign = function (id) {
+    objectives[id].changeAssigned(true);
+  };
+  
+  self.getStatus = function (id) {
+    return objectives[id].completed;
   };
   
   //returns a subset of info about items which have already been assigned
   self.getAll = function () {
     let minilist = [];
-    for (let i = 0, l = objectives.length; i < l; i += 1) {
-      if (objectives[i].assigned) {
-        minilist.push({"text": objectives[i].text, "completed": objectives[i].completed});
+    for (let x in objectives) {
+      if (objectives[x].assigned && !objectives[x].failed) {
+        minilist.push({"text": objectives[x].text, "type": objectives[x].type, "completed": objectives[x].completed});
       }
     }
     return minilist;
