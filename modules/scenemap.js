39185ed20b1c954d07104097bb189ff695002355
GwenSW <gwen.simswilliams@gmail.com>
2017-12-09 16:17:20 +0000

    Parse coordinates string into x and y numbers

--- modules/scenemap.js
+++ modules/scenemap.js
@@ -8,6 +8,16 @@ const Scenemap = (function () {
   
   return {
     
+    updateLocation (co) {
+      let xy = __coordinates[co] || co,
+          x,
+          y;
+      xy = xy.split(" ", 2);
+      x = parseInt(xy[0]);
+      y = parseInt(xy[1]);
+      console.log(x, y);
+    },
+    
     set: function (filename) {
       View.setMap(filename);
     },
